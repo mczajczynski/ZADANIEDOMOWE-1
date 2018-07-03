3018e1fa9c03ccb7524db6bc94dd5b96dddeb641
Maciej <m.czajczynski28@gmail.com>
2018-07-03 19:44:07 +0200

    Dodanie drugiego commita

--- src/Uczesie.java
+++ src/Uczesie.java
@@ -4,5 +4,7 @@ public class Uczesie {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }
