f6f7e7db889d20d9cdcec781d41c491bde1331a5
Maciej <m.czajczynski28@gmail.com>
2018-07-03 19:41:48 +0200

    Dodanie pierwszego commita

---
+++ src/Uczesie.java
@@ -0,0 +1,8 @@
+public class Uczesie {
+
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+    }
+}
